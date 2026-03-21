a5795f84918b7a9f56dcfc9775561219ed02e8a5
unknown <akshaykrish533516@gmail.com>
2026-03-21 13:28:49 +0530

    Solved recursive fibo,factorial problems

---
+++ recursiveFactorial.js
@@ -0,0 +1,9 @@
+function recursiveFactorial(n){
+    if(n===0 ||n===1){
+        return 1;
+    }
+    return n*recursiveFactorial(n-1);
+}
+
+console.log(recursiveFactorial(4));
+console.log(recursiveFactorial(5));
