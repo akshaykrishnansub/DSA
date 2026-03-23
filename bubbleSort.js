422760b74b5bcf246e78cad82bf1ac45da5d7900
unknown <akshaykrish533516@gmail.com>
2026-03-23 17:43:28 +0530

    Solved Bubble sort logic

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr){
+    let swapped;
+    do{
+        swapped=false;
+        for(let i=0;i<arr.length-1;i++){
+        if(arr[i]>arr[i+1]){
+            let temp=arr[i]; //use temporary variable
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+            swapped=true;
+        }
+    }
+    }while(swapped)
+}
+
+const arr=[-6,-2,8,4,20,1,1];
+bubbleSort(arr);
+console.log(arr);
